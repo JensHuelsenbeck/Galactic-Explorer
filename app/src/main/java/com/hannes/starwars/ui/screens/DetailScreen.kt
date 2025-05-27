a0625d72969b71727388d95b87ee9bd9164d4965
RebeccaCalabretta <rebecca.calabretta@gmail.com>
2025-05-27 12:05:13 +0200

    edit DetailScreen

--- app/src/main/java/com/hannes/starwars/ui/screens/DetailScreen.kt
+++ app/src/main/java/com/hannes/starwars/ui/screens/DetailScreen.kt
@@ -3,15 +3,25 @@ package com.hannes.starwars.ui.screens
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.hannes.starwars.R
+import com.hannes.starwars.data.datasource.dummyCharacters
+import com.hannes.starwars.data.datasource.dummyMovie
 import com.hannes.starwars.ui.components.CategoryTitle
+import com.hannes.starwars.ui.components.ListItem
+import com.hannes.starwars.ui.components.MovieList
 import com.hannes.starwars.ui.theme.basic
 import com.hannes.starwars.ui.theme.starWarsOrange
 import com.hannes.starwars.ui.theme.starwarsfont
@@ -19,34 +29,60 @@ import com.hannes.starwars.ui.theme.starwarsfont
 @Composable
 fun DetailScreen(modifier: Modifier = Modifier) {
     Column(
-        modifier = Modifier
+        modifier = Modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
             painter = painterResource(R.drawable.tatooine),
             contentDescription = null
         )
         Text(
-            text = "Tatooine",
+            text = "tatooine",
             fontFamily = basic,
             style = MaterialTheme.typography.headlineSmall,
             color = starWarsOrange,
         )
         Text(
-            text = "Tatooine",
+            text = "tatooine",
             fontFamily = starwarsfont,
             color = starWarsOrange,
             fontSize = 17.sp
         )
         Card() {
-            Row() {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "planet details",
+                    fontFamily = basic,
+                    color = starWarsOrange,
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier
+                        .padding(horizontal = 8.dp)
+                        .padding(top = 8.dp)
+                )
+
+                Text(
+                    text = " planet details",
+                    fontFamily = starwarsfont,
+                    color = starWarsOrange.copy(alpha = 0.5f),
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier
+                        .padding(horizontal = 8.dp)
+                        .padding(top = 8.dp)
+                )
+            }
+
+            Row(
+                modifier = Modifier
+                    .padding(horizontal = 8.dp, vertical = 4.dp)
+
+            ) {
                 Column {
-                    Text(
-                        text = "Planet Details",
-                        style = MaterialTheme.typography.bodyLarge
-                    )
+
                     Row {
                         Text(
-                            text = "rotation period"
+                            text = "rotation period: "
                         )
                         Text(
                             text = "23"
@@ -55,7 +91,7 @@ fun DetailScreen(modifier: Modifier = Modifier) {
 
                     Row {
                         Text(
-                            text = "orbital period"
+                            text = "orbital period: "
                         )
                         Text(
                             text = "304"
@@ -64,7 +100,7 @@ fun DetailScreen(modifier: Modifier = Modifier) {
 
                     Row {
                         Text(
-                            text = "Diameter"
+                            text = "diameter: "
                         )
                         Text(
                             text = "10465"
@@ -73,7 +109,7 @@ fun DetailScreen(modifier: Modifier = Modifier) {
 
                     Row {
                         Text(
-                            text = "climate"
+                            text = "climate: "
                         )
                         Text(
                             text = "arid"
@@ -82,11 +118,13 @@ fun DetailScreen(modifier: Modifier = Modifier) {
 
                 }
 
+                Spacer(Modifier.weight(1f))
+
                 Column {
 
                     Row {
                         Text(
-                            text = "gravity"
+                            text = "gravity: "
                         )
                         Text(
                             text = "1 standard"
@@ -95,7 +133,7 @@ fun DetailScreen(modifier: Modifier = Modifier) {
 
                     Row {
                         Text(
-                            text = "terrain"
+                            text = "terrain: "
                         )
                         Text(
                             text = "dessert"
@@ -104,7 +142,7 @@ fun DetailScreen(modifier: Modifier = Modifier) {
 
                     Row {
                         Text(
-                            text = "surface water"
+                            text = "surface water: "
                         )
                         Text(
                             text = "1%"
@@ -113,7 +151,7 @@ fun DetailScreen(modifier: Modifier = Modifier) {
 
                     Row {
                         Text(
-                            text = "Population"
+                            text = "population: "
                         )
                         Text(
                             text = "200000"
@@ -128,13 +166,12 @@ fun DetailScreen(modifier: Modifier = Modifier) {
             text = "residents"
         )
 
-        Column {
-            Row {
-                Text(
-                    text = "luke skywalker"
-                )
+        LazyColumn {
+            items(dummyCharacters) { char ->
+                ListItem(title = char.name, subTitle = char.birth_year)
             }
         }
+        MovieList(dummyMovie)
     }
 
 }
