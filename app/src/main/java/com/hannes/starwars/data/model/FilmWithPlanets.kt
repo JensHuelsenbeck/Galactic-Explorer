2e26420c7a4d4ac2660b72880f41fab5260c94b9
Tarlan Bakhtiari <64501123+tb110-star@users.noreply.github.com>
2025-05-28 12:10:23 +0200

    FilmWithPlanets

--- app/src/main/java/com/hannes/starwars/data/model/FilmWithPlanets.kt
+++ app/src/main/java/com/hannes/starwars/data/model/FilmWithPlanets.kt
@@ -3,18 +3,19 @@ package com.hannes.starwars.data.model
 import androidx.room.Embedded
 import androidx.room.Junction
 import androidx.room.Relation
+import com.hannes.starwars.data.model.crossref.FilmPlanetCrossRef
 
-/*
 data class FilmWithPlanets(
-    @Embedded val film: Film,
+    @Embedded
+    val film: Film,
     @Relation(
         parentColumn = "filmId",
         entityColumn = "planetId",
         associateBy = Junction(FilmPlanetCrossRef::class)
     )
+    val planets: List<Planet>
 )
 
- */
 /*
 data class PlaylistWithSongs(
    @Embedded val playlist: Playlist,
